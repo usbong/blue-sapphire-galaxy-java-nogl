ccb801a6fb31f14e07d81fb7faaa7d7e150abf23
SYSON, MICHAEL B. <masarapmabuhay@gmail.com>
2025-07-14 15:34:27 +0800

    Add files via upload

---
+++ javaNoGL/game.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Copyright 2024-2025 USBONG
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
+#
+# @company: USBONG
+# @author: SYSON, MICHAEL B.
+# @date created: 20240720
+# @date updated: 20250714; from 20240720
+#
+
+#cd "src"
+#java UsbongMain
+
+#java -classpath "./src" UsbongMain
+
+# "./javapath/OpenJDK8U-jre_x64_windows_hotspot_8u452b09/jdk8u452-b09-jre/bin/java" -jar "./UsbongMainWithSteamWorks4j.jar"  
+
+./runWithSteamWorks4jJarFile.sh
